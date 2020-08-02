750c30761e1f0e4d9e2c7d4d5bd6b8e2dc3aef4e
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-08-02 10:23:44 +0600

    array

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var person = {
     name: 'Sifat M',
-    age: 25
+    age: 25,
+    hobby: ['sports', 'programming']
 };
 console.log(person.name);
+for (var _i = 0, _a = person.hobby; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
 const person: {
     name: string;
     age: number;
+    hobby: string[];
 } = {
     name: 'Sifat M',
-    age: 25
+    age: 25,
+    hobby: ['sports', 'programming']
 }
 
 console.log(person.name)
+
+for (const hobby of person.hobby){
+    console.log(hobby)
+}
