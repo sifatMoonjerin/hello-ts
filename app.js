2edf7ee125f8a599f807636f8b900d15c959b718
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-08-02 10:33:02 +0600

    tuple

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var person = {
     name: 'Sifat M',
     age: 25,
-    hobby: ['sports', 'programming']
+    hobby: ['sports', 'programming'],
+    role: [3, 'author']
 };
 console.log(person.name);
+person.role.push('asdf');
+console.log(person.role);
 for (var _i = 0, _a = person.hobby; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
--- app.ts
+++ app.ts
@@ -2,13 +2,16 @@ const person: {
     name: string;
     age: number;
     hobby: string[];
+    role: [number, string];
 } = {
     name: 'Sifat M',
     age: 25,
-    hobby: ['sports', 'programming']
+    hobby: ['sports', 'programming'],
+    role: [3, 'author']
 }
 
 console.log(person.name)
+console.log(person.role)
 
 for (const hobby of person.hobby){
     console.log(hobby)
