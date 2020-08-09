1d50370e10c2eac735d2ffc1e83c5732b1453b27
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-08-09 10:26:33 +0600

    html template

--- public/app.js
+++ public/app.js
@@ -1,38 +1,21 @@
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
-// const invOne = new Invoice("mario", "phone", 5000);
-// const invTwo = new Invoice("goku", "bean", 1000);
-// const invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
-// const anchor = document.querySelector('a') as HTMLAnchorElement;
-// console.log(anchor.href)
 const form = document.querySelector("form");
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let doc;
-    if (type.value === 'invoice') {
+    if (type.value === "invoice") {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
-// interface Point {
-//   x: number;
-// }
-// const center: Point = {
-//   x: 1,
-//   y: 4,
-// };
-// interface Point {
-//   y: number;
-// }
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,7 @@
-import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
-
-// const invOne = new Invoice("mario", "phone", 5000);
-// const invTwo = new Invoice("goku", "bean", 1000);
-
-// const invoices: Invoice[] = [];
-
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
-
-// const anchor = document.querySelector('a') as HTMLAnchorElement;
-
-// console.log(anchor.href)
 
 const form = document.querySelector("form") as HTMLFormElement;
 
@@ -25,26 +10,19 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (event: Event): void => {
   event.preventDefault();
+
   let doc: HasFormatter;
-  if (type.value === 'invoice'){
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
-  }else{
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+  
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  console.log(doc);
+
+  list.render(doc, type.value, "end");
 });
-
-// interface Point {
-//   x: number;
-// }
-
-// const center: Point = {
-//   x: 1,
-//   y: 4,
-// };
-
-// interface Point {
-//   y: number;
-// }
